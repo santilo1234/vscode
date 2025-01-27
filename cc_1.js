89d9cc3e6a393d6fa19cefc558cda4060ecf92e2
Santiago Lopez <tiagolo1234@gmail.com>
2025-01-26 20:34:41 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "John Doe";
+const employeeID = 12345;
+var isActive = true;
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
