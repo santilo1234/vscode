8081362d0f479276110b4f1f2e4c6c71ab9cfb76
Santiago Lopez <tiagolo1234@gmail.com>
2025-01-26 20:36:56 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true;
 console.log(employeeName, typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
+
+let productName = "Wireless Mouse";
+const productPrice = 29.99;
+var isAvailable = true;
+
+console.log(productName, typeof productName);
+console.log(productPrice, typeof productPrice);
+console.log(isAvailable, typeof isAvailable);
