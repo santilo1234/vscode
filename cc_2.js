69c1d13403fb1473c6a71ed5e80717c2bc16f4b9
Santiago Lopez <tiagolo1234@gmail.com>
2025-01-26 22:28:33 -0500

    Task 2 - Student Score

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,12 @@ let products = ["Apple", "Banana", "Orange", "Mango", "Pineapple"];
 products.push("Grapes");
 products.pop();
 console.log(products);
+
+let scores = [85, 90, 78, 92, 88];
+scores[1] = 95;
+
+let averageScore = scores.reduce((total, score) => total + score, 0) / scores.length;
+
+
+console.log("Updated Scores:", scores);
+console.log("Average Score:", averageScore);
